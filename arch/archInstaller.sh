2faf9c2235cbbd893f96eef25352e0acbcb99690
sumanta23 <nandisumanta0@gmail.com>
2021-08-08 22:49:09 +0530

    save

--- arch/archInstaller.sh
+++ arch/archInstaller.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
+hostname="basehost"
+locale="en_US"
+dev="/dev/sda"
+
 setntp(){
+   echo "setting ntp"
    timedatectl set-ntp true
 }
 
 chooseeditor(){
-	options=()
-	options+=("nano" "")
-	options+=("vim" "")
-	options+=("vi" "")
-	options+=("edit" "")
-	sel=$(whiptail --backtitle "${apptitle}" --title "${txteditor}" --menu "" 0 0 0 \
-		"${options[@]}" \
-		3>&1 1>&2 2>&3)
-	if [ "$?" = "0" ]; then
-		clear
-		echo "export EDITOR=${sel}"
-		export EDITOR=${sel}
-		EDITOR=${sel}
-		pressanykey
-	fi
+    echo "setting editor"
+    echo "export EDITOR=vim"
+    export EDITOR=vim
+    EDITOR=vim
+}
+
+syncmirror(){
+    echo "syncing  mirror"
+    pacman -Syy
 }
 
 rebootpc(){
@@ -29,32 +28,92 @@ rebootpc(){
 	fi
 }
 
-selectdisk(){
-		items=$(lsblk -d -p -n -l -o NAME,SIZE -e 7,11)
-		options=()
-		IFS_ORIG=$IFS
-		IFS=$'\n'
-		for item in ${items}
-		do  
-				options+=("${item}" "")
-		done
-		IFS=$IFS_ORIG
-		result=$(whiptail --backtitle "${APPTITLE}" --title "${1}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
-		if [ "$?" != "0" ]
-		then
-				return 1
-		fi
-		echo ${result%%\ *}
-		return 0    
-}
 
 diskpartition(){
-    dev=$1 
-    printf "o\nn\np\n1\n\n\nw\n" | sudo fdisk "$dev"
-    //printf "n\np\n1\n+1G\nn\n2\n\n\nw\n" | sudo fdisk "$dev"
-    sudo mkfs.ext4 "${dev}1"
+    printf "n\np\n1\n\n+1G\nn\n2\n\n\nw\n" | sudo fdisk "$dev"
+    mkfs.ext4 "${dev}2"
+    mkswap "${dev}1"
 }
 
-setntp
-chooseeditor
-selectdisk
+mountdrive(){
+    echo "mounting drives"
+    mount /dev/sda2 /mnt
+    swapon /dev/sda1
+}
+
+unmountdrive(){
+    echo "unmounting drive"
+    umount -R /mnt
+}
+
+dopacstrap(){
+    echo "pacstrap /mnt base linux linux-firmware vim"
+    pacstrap /mnt base linux linux-firmware vim
+}
+
+dogenfstab(){
+    echo "genfstab"
+    genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+dochroot(){
+    echo "chrooting /mnt"
+    arch-chroot /mnt
+}
+
+settimezone(){
+    echo "setting timezone"
+    ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+    hwclock --systohc
+}
+
+localegen(){
+    echo "generating lacale"
+    sed -i '/#'${locale}'.UTF-8/s/^#//g' /etc/locale.gen
+    echo "LANG=${locale}.UTF-8" > /etc/locale.conf
+    locale-gen
+    
+}
+
+sethostname(){
+    echo "setting hostname"
+    echo "${hostname}" > /etc/hostname
+}
+
+makeinitcpio(){
+    echo "mkinitcpio -P"
+    mkinitcpio -P
+}
+
+setrootpasswd(){
+    echo "setting root passwd"
+    passwd root
+}
+
+grubinstall(){
+    echo "installing grub"
+    pacman -S grub
+    grub-install /dev/sda
+    grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+tillchroot(){
+    setntp
+    chooseeditor
+    #diskpartition
+    mountdrive
+    syncmirror
+    dopacstrap
+    dogenfstab
+    dochroot
+}
+
+postchroot(){
+    settimezone
+    localegen
+    sethostname
+    makeinitcpio
+    setrootpasswd
+    grubinstall
+    unmountdrive
+}
