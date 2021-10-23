8e86b7f4eedd4931292bc2437b77b19d93230817
sumanta23 <nandisumanta0@gmail.com>
2021-10-23 16:58:46 +0530

    restructured

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@
 #create folders
 mkdir -p ~/.m2
 
-ln -s ${PWD}/bashrc ~/.bashrc
-ln -s ${PWD}/bash_aliases ~/.bash_aliases
-ln -s ${PWD}/vimrc ~/.vimrc
-ln -s ${PWD}/gitconfig ~/.gitconfig
-ln -s ${PWD}/gitignore ~/.gitignore
-ln -s ${PWD}/screenrc ~/.screenrc
-ln -s ${PWD}/tmux.conf ~/.tmux.conf
-ln -s ${PWD}/settings.xml ~/.m2/settings.xml
-ln -s ${PWD}/pmdrules.xml ~/.m2/pmdrules.xml
+ln -s -f ${PWD}/bashconfig/bashrc ~/.bashrc
+ln -s -f ${PWD}/bashconfig/bash_aliases ~/.bash_aliases
+ln -s -f ${PWD}/vim/vimrc ~/.vimrc
+ln -s -f ${PWD}/git/gitconfig ~/.gitconfig
+ln -s -f ${PWD}/git/gitignore ~/.gitignore
+ln -s -f ${PWD}/screen/screenrc ~/.screenrc
+ln -s -f ${PWD}/tmux/tmux.conf ~/.tmux.conf
+ln -s -f ${PWD}/lang/settings.xml ~/.m2/settings.xml
+ln -s -f ${PWD}/lang/pmdrules.xml ~/.m2/pmdrules.xml
 
 ln -s ${PWD}/pbin ~/pbin
 
@@ -20,13 +20,13 @@ ln -s ${PWD}/pbin ~/pbin
 
 os=$(uname -s)
 if [ $os == "Darwin" ]; then
-    # do mac specific installtion stuff
+	echo do mac specific installtion stuff
 else
-    mkdir -p ~/.themes
-    ln -s ${PWD}/xinitrc ~/.xinitrc
-    ln -s ${PWD}/flat-theme ~/.themes/flat
-    ln -s ${PWD}/Xresources ~/.Xresources
-    ln -s ${PWD}/gtkrc-2.0 ~/.gtkrc-2.0
+	mkdir -p ~/.themes
+	ln -s ${PWD}/linux/xinitrc ~/.xinitrc
+	ln -s ${PWD}/flat-theme ~/.themes/flat
+	ln -s -f ${PWD}/linux/Xresources ~/.Xresources
+	ln -s -f ${PWD}/linux/gtkrc-2.0 ~/.gtkrc-2.0
 fi
 
 echo "installing ngrok..."
