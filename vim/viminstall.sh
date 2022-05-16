c8768a7788e7d820a8c99eff4da2a0771cf61691
sumanta23 <nandisumanta0@gmail.com>
2022-05-16 16:23:26 +0530

    color code

--- vim/viminstall.sh
+++ vim/viminstall.sh
@@ -2,11 +2,12 @@
 
 
 #install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle  ~/.vim/.vimtemp/swaps ~/.vim/.vimtemp/backups ~/.vim/.vimtemp/undo
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors  ~/.vim/.vimtemp/swaps ~/.vim/.vimtemp/backups ~/.vim/.vimtemp/undo
 curl -k -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
+cp monokai.vim ~/.vim/colors
 #install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 
 #install ctags
 #sudo apt-get install ctags
